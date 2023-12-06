25aff569ccbcb8ec8f61cd25535454d1557c62c5
Nicolas Sauzede <nicolas.sauzede@gmail.com>
2023-12-06 09:05:33 +0100

    Add python part2

--- vade/src/2023/day06/_test.py
+++ vade/src/2023/day06/_test.py
@@ -1,38 +1,38 @@
 import unittest
-INP01_="""Time:      7  15   30
-Distance:  9  40  200"""
 INP01=[[7,9],[15,40],[30,200]]
 RES01=288
-INP1_="""Time:        41     77     70     96
-Distance:   249   1362   1127   1011"""
 INP1=[[41,249],[77,1362],[70,1127],[96,1011]]
 RES1=771628
 INP02=[[71530,940200]]
 INP2=[[41777096,249136211271011]]
 RES02=71503
-RES2=0
-def compute(part:int, inp:dict)->int:
+RES2=27363861
+def compute(inp:dict)->int:
     res = 1
-    wins=[]
     for (tt,drec) in inp:
-        print(f"tt={tt} drec={drec}")
         w=0
-        for th in range(tt):
-            s=th
-            d=s*(tt-th)
+        for th1 in range(tt):
+            s=th1
+            d=s*(tt-th1)
             if d>drec:
-                w+=1
-            #print(f"th={th} s={s} d={d}")
-        wins+=[w]
+                break
+        for th2 in range(tt-1,-1,-1):
+            s=th2
+            d=s*(tt-th2)
+            if d>drec:
+                break
+        if th2>th1:
+            w=th2-th1+1
         res*=w
-    print(f"res={res} wins={wins}")
     return res
 class T000(unittest.TestCase):
-    def Ztest_2000(self):
-        self.assertEqual(RES2,compute(0, INP2))
+    def test_2000(self):
+        self.assertEqual(RES2,compute(INP2))
     def test_0200(self):
-        self.assertEqual(RES02,compute(1, INP02))
+        self.assertEqual(RES02,compute(INP02))
     def test_0100(self):
-        self.assertEqual(RES01,compute(0, INP01))
+        self.assertEqual(RES01,compute(INP01))
+    def test_0101(self):
+        self.assertEqual(RES01,compute(INP01))
     def test_1000(self):
-        self.assertEqual(RES1,compute(0, INP1))
+        self.assertEqual(RES1,compute(INP1))
