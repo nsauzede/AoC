fc43d0956527cd4af6efed2297ad041b15933d85
Nicolas Sauzede <nicolas.sauzede@gmail.com>
2023-12-11 19:01:03 +0100

    Add day11 python part2

--- vade/src/2023/day11/_test.py
+++ vade/src/2023/day11/_test.py
@@ -21,11 +21,38 @@ INP01_=[['.','.','.','#','.','.','.','.','.','.',],
 ['#','.','.','.','#','.','.','.','.','.',]]
 RES01=374
 RES1=10077850
-RES2=0
+RES11=1030
+RES12=8410
+RES2=504715068438
 def disp(inp,text="inp"):
     print(f"{text}:")
     for r in inp:
         print(" ".join(r))
+def expand1(inp:list,mult=2)->list:
+    disp(inp, "INITIAL")
+    erows=emptyrows(inp)
+    print(f"erows={erows}")
+    ecols=emptycols(inp)
+    print(f"ecols={ecols}")
+    gal=[]
+    y=0
+    for j,r in enumerate(inp):
+        r0=[]
+        if j in erows:
+            y+=mult-1
+        x=0
+        for i,c in enumerate(r):
+            if i in ecols:
+                x+=mult-1
+            if c=='#':
+                gal+=[(y,x)]
+            x+=1
+        y+=1
+    inp2=[]
+    print(f"y={y} x={x}")
+    disp(inp2,"FINAL")
+    print(f"gal={gal}")
+    return gal
 def expand(inp:list)->list:
     disp(inp, "INITIAL")
     inp0=inp.copy()
@@ -64,15 +91,19 @@ def expand(inp:list)->list:
 def manh(a,b):
     n=0
     if a!=b:
-        delta=b-a
-        step=delta//abs(delta)
-        for i in range(a,b,step):n+=1
+        #delta=b-a
+        #step=delta//abs(delta)
+        #for i in range(a,b,step):n+=1
+        n+=abs(b-a)
     return n
 # returns manhattan distance between coords
 def manhattan(coord1:tuple, coord2=(0,0))->int:
     return manh(coord1[1],coord2[1])+manh(coord1[0],coord2[0])
-def compute(inp:list,part=0,cnt='B')->int:
-    gal=expand(inp)
+def compute(inp:list,part=0,mult=2)->int:
+    if part==1:
+        gal=expand1(inp,mult)
+    else:
+        gal=expand(inp)
     res = 0
     for i,g1 in enumerate(gal):
         for g2 in gal[i+1:]:
@@ -105,6 +136,14 @@ def parse(inp):
 class T000(unittest.TestCase):
     def test_01_0(self):
         self.assertEqual(RES01+0,compute(INP01_))
+    def test_1100_(self):
+        self.assertEqual(RES01+0,compute(INP01_, 1))
+    def test_1100(self):
+        self.assertEqual(RES11+0,compute(INP01_, 1, 10))
+    def test_1200(self):
+        self.assertEqual(RES12+0,compute(INP01_, 1, 100))
+    def test_2000(self):
+        self.assertEqual(RES2+0,compute(parse(open("input1","rt").read()), 1, 1000000))
     def test_0100(self):
         self.assertEqual(RES01+0,compute(parse(INP01)))
     def test_1000(self):
--- vade/src/2023/day11/puzzle.md
+++ vade/src/2023/day11/puzzle.md
@@ -113,8 +113,6 @@ Expand the universe, then find the length of the shortest path between every pai
 
 Your puzzle answer was `10077850`.
 
-The first half of this puzzle is complete! It provides one gold star: \*
-
 \--- Part Two ---
 ----------
 
@@ -126,8 +124,12 @@ Now, instead of the expansion you did before, make each empty row or column *one
 
 Starting with the same initial image, expand the universe according to these new rules, then find the length of the shortest path between every pair of galaxies. *What is the sum of these lengths?*
 
-Answer:
+Your puzzle answer was `504715068438`.
 
-Although it hasn't changed, you can still [get your puzzle input](11/input).
+Both parts of this puzzle are complete! They provide two gold stars: \*\*
 
-You can also [Shareon [Twitter](https://twitter.com/intent/tweet?text=I%27ve+completed+Part+One+of+%22Cosmic+Expansion%22+%2D+Day+11+%2D+Advent+of+Code+2023&url=https%3A%2F%2Fadventofcode%2Ecom%2F2023%2Fday%2F11&related=ericwastl&hashtags=AdventOfCode) [Mastodon](javascript:void(0);)] this puzzle.
+At this point, you should [return to your Advent calendar](/2023) and try another puzzle.
+
+If you still want to see it, you can [get your puzzle input](11/input).
+
+You can also [Shareon [Twitter](https://twitter.com/intent/tweet?text=I%27ve+completed+%22Cosmic+Expansion%22+%2D+Day+11+%2D+Advent+of+Code+2023&url=https%3A%2F%2Fadventofcode%2Ecom%2F2023%2Fday%2F11&related=ericwastl&hashtags=AdventOfCode) [Mastodon](javascript:void(0);)] this puzzle.
